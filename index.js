b872a070426ad7a9424a85d5ae4caaeb37be9d99
Aouf <aoufmomin02@gmail.com>
2023-05-26 00:29:29 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let mileageb = document.getElementById("mileage");
+let distanceb = document.getElementById("distance");
+let priceb= document.getElementById("price");
+let boxb = document.getElementById("box");
+let button = document.getElementById("button");
+
+button.onclick = () => {
+    if(mileageb.value=== ""|| distanceb.value===""|| priceb.value===""){
+        boxb.innerText="Please enter some values"
+    }
+    else{
+  let a = parseFloat(distanceb.value);
+  let b = parseFloat(mileageb.value);
+  let c= parseFloat(priceb.value);
+  if (a <= 0 || b <= 0|| c<=0) {
+    boxb.innerText = "Please Enter positive values";
+  } else {
+    console.log(a / b);
+    let d = a / b;
+    boxb.innerText = `It will cost Rs.${d*c} to drive ${a} kilometers`;
+  }}
+};
